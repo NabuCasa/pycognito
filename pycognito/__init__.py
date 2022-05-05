994a99537b4b2c71d4721edf9b7080786cb4a543
Thibaut Juzeau <39088822+Thiebosh@users.noreply.github.com>
2022-05-05 16:27:25 +0200

    fix black formatter

--- pycognito/__init__.py
+++ pycognito/__init__.py
@@ -728,7 +728,9 @@ class Cognito:
         Helper function to verify and set token attributes based on a Cognito
         AuthenticationResult.
         """
-        self.verify_token(tokens["AuthenticationResult"]["AccessToken"], "access_token", "access")
+        self.verify_token(
+            tokens["AuthenticationResult"]["AccessToken"], "access_token", "access"
+        )
         self.verify_token(tokens["AuthenticationResult"]["IdToken"], "id_token", "id")
         if "RefreshToken" in tokens["AuthenticationResult"]:
             self.refresh_token = tokens["AuthenticationResult"]["RefreshToken"]
